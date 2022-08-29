e340f60fcda9dbcb615181ae7658bb4bccc61206
TypeChecked <camrenmumme@gmail.com>
2022-08-28 22:58:25 -0400

    Make Literal AST node non-abstract

--- src/Compiler/AST/Nodes/Literals/Literal.ts
+++ src/Compiler/AST/Nodes/Literals/Literal.ts
@@ -1,6 +1,6 @@
 import { Node, NodeType } from "../../Utils"
 
-export abstract class Literal extends Node {
+export class Literal extends Node {
     public Type: NodeType = NodeType.Unknown
 
     constructor() {
