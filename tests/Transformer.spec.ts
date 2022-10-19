747df96226fd46cc9e71b73092a55b969c07d65c
TypeChecked <camrenmumme@gmail.com>
2022-10-19 19:40:39 -0400

    Unit Testing - Remove explicit any types

--- tests/Transformer.spec.ts
+++ tests/Transformer.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it } from "mocha"
-import { Lex, Parse, Mutator } from "../src"
+import { Lex, Parse, Mutator, Array, Luau } from "../src"
 import { readFileSync } from "fs"
 import { resolve } from "path"
 import { expect } from "chai"
@@ -9,10 +9,10 @@ describe("Transformer", () => {
         const LexedTokens = Lex(readFileSync(resolve("./tests/resources/ArrayTest.rose"), { encoding: "utf8" }))
         const ParsedAST = await Parse(LexedTokens)
 
-        console.log((ParsedAST[0] as any).Value[5])
+        console.log((ParsedAST[0] as Array).Value[5])
         expect(ParsedAST[0]).to.exist
         if(!ParsedAST[0]) return
         const TransformedAST = await Mutator.Mutate(ParsedAST[0])
-        console.log((TransformedAST as any).Value[5])
+        console.log((TransformedAST as Luau.Array).Value[5])
     })
 })
